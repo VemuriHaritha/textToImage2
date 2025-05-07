e92e004f98750ebc726ddeb6ac4f0eac65ffda09
Vemuri Haritha <161494669+VemuriHaritha@users.noreply.github.com>
2025-05-07 10:53:46 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,13 +2,12 @@ import streamlit as st
 import requests
 from PIL import Image
 from io import BytesIO
-# Streamlit title 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
-st.title('A free Text-to-Image Generator using Stable Diffusion API By Haritha')
+st.title('Text-to-Image Generator using Stable Diffusion API by Haritha V')
 prompt = st.text_input("Enter a text prompt:", value="A serene beach at sunset")
 if st.button("Generate Image"):
-    API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
+    API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
     headers = {
-        "Authorization": "Bearer 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" 
+        "Authorization": "Bearer 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"  
     }
     data = {
         "inputs": prompt
@@ -16,8 +15,9 @@ if st.button("Generate Image"):
     response = requests.post(API_URL, headers=headers, json=data)
 
     if response.status_code == 200:
-        image_data = response.content 
+        image_data = response.content  
         img = Image.open(BytesIO(image_data))
         st.image(img, caption=f"Generated image for: {prompt}", use_column_width=True)
     else:
-        st.error(f"Failed to generate image: {response.text}")
+        st.error(f"Failed to generate image: {response}")
+    response = requests.post(API_URL, headers=headers, json=data)
